cdcab6531b7cd23171d18b42895d1d1bc7eb095e
wwtall1 <hankklein13@yahoo.com>
2023-05-11 22:52:16 -0500

    Add files via upload
    Puts maximums in HTML to match SQL in add_animal page.

--- animal-monitoring_ENT-NET/monitor_manager/add_animal.php
+++ animal-monitoring_ENT-NET/monitor_manager/add_animal.php
@@ -5,10 +5,10 @@
 <fieldset>
     <legend>Add Animal Type</legend>
     <label for='type'>Animal Type/Common Name</label>
-    <input type="text" name="type" id="type" value=""><br><br>
+    <input type="text" name="type" id="type" value="" maxlength="15"><br><br>
     <br><br>
     <label for='food'>Recommended Food: </label>
-    <input type="text" name="food" id="food" value=""><br><br>
+    <input type="text" name="food" id="food" value="" maxlength="100"><br><br>
     
     <input type="submit" value="Submit"><br><br><br>
 </fieldset>
